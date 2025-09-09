f69cef9da5c00b6a36fed2c4734c484eda0282c9
guziqian03 <z.gu.13@student.scu.edu.au>
2025-09-10 07:35:39 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root', // 如果你有设置密码请替换
-  password: 'Lunyiyao20030110.', // 或你的本地 root 密码
+  password: 'Guziqian030121', // 或你的本地 root 密码
   database: 'sleepytiger',
   waitForConnections: true,
   connectionLimit: 10,
